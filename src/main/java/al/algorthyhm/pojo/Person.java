a8bc8545f214c29d9956ad4d70fb8278a83de842
Lejrat Mesi <lejrat.mesi@base-com.al>
2023-03-23 10:05:49 +0100

    Minor bug fixes

--- src/main/java/al/algorthyhm/pojo/Person.java
+++ src/main/java/al/algorthyhm/pojo/Person.java
@@ -14,17 +14,10 @@ public class Person {
         List<String> emailCombinations = new ArrayList<>();
 
         emailCombinations.add(emri.toLowerCase().charAt(0)+ "." + mbiemri.toLowerCase() + "@" + providers.getProvider());
-        emailCombinations.add(emri.toLowerCase() + "." + mbiemri.toLowerCase() + "@" +  providers.getProvider());
-        emailCombinations.add(emri.toLowerCase() + mbiemri.toLowerCase() + "@"+ providers.getProvider());
+       emailCombinations.add(emri.toLowerCase() + mbiemri.toLowerCase() + "@"+ providers.getProvider());
         emailCombinations.add(emri.charAt(0)+ mbiemri.toLowerCase() + "@"+ providers.getProvider());
         emailCombinations.add(mbiemri.toLowerCase() + "." + emri.toLowerCase() + "@"+ providers.getProvider());
-        emailCombinations.add(mbiemri.toLowerCase() + emri.toLowerCase() + "@"+ providers.getProvider());
         emailCombinations.add(emri.charAt(0)+ "." + mbiemri + "@" + providers.getProvider());
-        emailCombinations.add(emri + "." + mbiemri + "@" +  providers.getProvider());
-        emailCombinations.add(emri + mbiemri + "@"+ providers.getProvider());
-        emailCombinations.add(emri.charAt(0)+ mbiemri + "@"+ providers.getProvider());
-        emailCombinations.add(mbiemri + "." + emri + "@"+ providers.getProvider());
-        emailCombinations.add(mbiemri + emri + "@"+ providers.getProvider());
 
         return emailCombinations;
     }
